7c979d1e9d1d27db5550015cfb1e8da83a050423
dengchenyang.brady <3013506862@qq.com>
2022-01-16 20:37:59 +0800

    fix writing error

--- src/main/java/com/example/boottest/model/UserModel.java
+++ src/main/java/com/example/boottest/model/UserModel.java
@@ -1,7 +1,6 @@
 package com.example.boottest.model;
 
 import com.baomidou.mybatisplus.annotation.IdType;
-import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.AllArgsConstructor;
@@ -14,7 +13,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @TableName("user")
-public class Usermodel {
+public class UserModel {
 
     @TableId(type = IdType.AUTO)
     private int id;
